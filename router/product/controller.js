060a7e7aee7bd2877769740e03ab49a26e3b433e
yaninamontella <yani_montella@hotmail.com>
2019-09-29 20:26:26 -0300

    first commit

---
+++ router/product/controller.js
@@ -0,0 +1,32 @@
+const getAll = (req, res) => {
+    res.send({data : true})
+}
+
+const getById = (req, res) => {
+    res.send({data : true})
+}
+
+const insert = (req, res) => {
+    res.send({data : true})
+}
+
+const upsert = (req, res) => {
+    res.send({data : true})
+}
+
+const update = (req, res) => {
+    res.send({data : true})
+}
+
+const remove = (req, res) => {
+    res.send({data : true})
+}
+
+module.exports = {
+    getAll,
+    getById,
+    insert,
+    upsert,
+    update, 
+    remove
+}
---
+++ router/user/controller.js
@@ -0,0 +1,34 @@
+const users = require('../../data/users.json')
+
+const getAll = (req, res) => {
+    res.send(users)
+}
+
+const getById = (req, res) => {
+    res.send({data : true})
+}
+
+const insert = (req, res) => {
+    res.send({data : true})
+}
+
+const upsert = (req, res) => {
+    res.send({data : true})
+}
+
+const update = (req, res) => {
+    res.send({data : true})
+}
+
+const remove = (req, res) => {
+    res.send({data : true})
+}
+
+module.exports = {
+    getAll,
+    getById,
+    insert,
+    upsert,
+    update, 
+    remove
+}
