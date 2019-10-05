ecba868aecae757d41edb325f821fde13548045c
yaninamontella <yani_montella@hotmail.com>
2019-10-05 12:57:38 -0300

    add  all methods of products and users

--- router/user/controller.js
+++ router/user/controller.js
@@ -1,51 +1,56 @@
 const users = require('../../data/users.json')
-/*const bodyParser = require('body-parser')*/
 
 const getAll = (req, res) => {
     res.send(users)
 }
 
 const getById = (req, res) => {
-    let paramId = Number(req.params.id)
+    const paramId = Number(req.params.id)
     const result = users.find( ({id}) => id === paramId)  
     res.send(result)
 }
 
 const insert = (req, res) => { 
-  let paramId = Number(req.body.id)
-  const result = users.find( ({id}) => id === paramId)   
-  if (result!=undefined){
-    res.send('Already exist a User with that id')
-  }
-  else {
+  const paramId = parseInt(req.body.id)
+  let user= users.find( ({id}) => id === paramId) 
+  if(user){
+    res.status(404).send({message: 'The User Already exist'})
+  }else{
     users.push(req.body)
-    res.send(`New user added: ${req.body.id} - ${req.body.name}`)
-  }  
+    res.send({message: 'New user added' , data : users[paramId]})
+    }
 }
 
 const upsert = (req, res) => {
-  let paramId = parseInt(req.params.id)
-  let index= users.findIndex(element => element.id==paramId)
-  if(index==-1){
-    res.send("Don't Exist User:\n:" + JSON.stringify(req.body, null, 4));
+  const paramId = parseInt(req.params.id)
+  let user= users.find( ({id}) => id === paramId) 
+  if(user){
+    user = { ...req.body, id: paramId}
+    res.send({message: 'Update Successfully!'});
   }else{
-    users.splice(index, 1, req.body)
-    res.send("Update Successfully! \n" + JSON.stringify(req.body, null, 4));
-  }
+    res.status(404).send({message: 'User not found'})
+    }
 }
 
 const update = (req, res) => {
-    res.send({data : true})
+  const user = users.find(user => user.id == req.params.id);
+  if (user) {
+    user[Object.keys(req.body)] = req.body[Object.keys(req.body)];
+    res.send(user);
+  }
+  else {
+    res.send('user doesn´t exist');
+  }
 }
 
 const remove = (req, res) => {
   let paramId = parseInt(req.params.id)
   let index= users.findIndex(element => element.id==paramId)
-  if(index==-1){
-    res.send("Don't Exist User:\n:" + JSON.stringify(req.body, null, 4));
+  if(index===-1){
+    res.status(404).send({message: 'User not found'})
   }else{
     users.splice(index, 1)
-    res.send("Delete Successfully! \n" + JSON.stringify(req.body, null, 4));
+    res.send({message: 'Delete Successfully!'});
   }
 }
 
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = require('./router')
+const bodyParser = require('body-parser')
+
 
 const app = express()
 const port = 5000
 
 app.use('/api', router)
 app.use(express.static('public'))
+app.use(bodyParser.json())
 
 
 
